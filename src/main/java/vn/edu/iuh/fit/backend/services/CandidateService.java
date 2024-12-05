9b6f99fcb18fc8f2afed03840bd2367968d6bb11
Trần Trung Hiếu <trantrunghieu.mail@gmail.com>
2024-12-06 00:35:14 +0700

    Build UI for web

---
+++ src/main/java/vn/edu/iuh/fit/backend/services/CandidateService.java
@@ -0,0 +1,20 @@
+package vn.edu.iuh.fit.backend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.Service;
+import vn.edu.iuh.fit.backend.models.Candidate;
+import vn.edu.iuh.fit.backend.repositories.CandidateRepository;
+
+@Service
+public class CandidateService {
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public Page<Candidate> findAll(int pageNo, int pageSize, String sortBy,
+                                   String sortDirection) {
+        Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+        return candidateRepository.findAll(pageable);
+    }
+}
